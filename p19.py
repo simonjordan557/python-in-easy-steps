f1f0d4cc0d6fabb7d11f09dcdd8dd37c8006a2de
= <simonjordan557@aol.com>
2021-04-19 11:51:40 +0100

    Page 21

--- p19.py
+++ p19.py
@@ -1,8 +1,12 @@
+# Initialise a variable with an integer value.
 var = 8
 print(var)
+# Assign a float value to the variable.
 var = 3.142
 print(var)
+# Assign a string value to the variable.
 var = 'Python In Easy Steps'
 print(var)
+# Assign a Boolean value to the variable.
 var = True
 print(var)
---
+++ p21.py
@@ -0,0 +1,4 @@
+# Initialise another variable with a user-specified value.
+lang = input("Favourite programming language? : ")
+# Output a string and a variable value.
+print(lang, "Is", "Fun", sep=" * ", end="!\n")
