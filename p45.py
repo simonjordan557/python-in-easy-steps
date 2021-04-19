a9a8a5acc8108378617ae6c025c49ae61470bb9e
= <simonjordan557@aol.com>
2021-04-19 13:24:58 +0100

    Page 45

---
+++ p45.py
@@ -0,0 +1,11 @@
+quarter = [ "January", "February", "March" ]
+print("First Month :", quarter[0])
+print("Second Month :", quarter[1])
+print("Third Month :", quarter[2])
+
+coords = [ [1, 2, 3], [4, 5, 6] ]
+print("Top Left :", coords[0][0])
+print("Bottom Right :", coords[1][2])
+
+print("Second Month, First Letter :", quarter[1][0])
+
