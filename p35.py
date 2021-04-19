5c086bdaa20a7cb2eab5c826509100d353c7de86
= <simonjordan557@aol.com>
2021-04-19 12:38:38 +0100

    Page 35

---
+++ p35.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+print("\nVariable a is :", "One" if (a == 1) else "Not One")
+print("Variable a is :", "Even" if (a % 2 == 0) else "Odd")
+
+max = a if (a > b) else b
+print("Greater Value Is :", max)
+
