97898222edc493c69c99b0d724c9402bfbeaabc2
= <simonjordan557@aol.com>
2021-05-04 13:42:48 +0100

    Completed p73

---
+++ p72.py
@@ -0,0 +1,18 @@
+def incrementer():
+	i = 1
+	while True:
+		yield i
+		i += 1
+
+inc = incrementer()
+
+print(next(inc))
+print(next(inc))
+print(next(inc))
+print(next(inc))
+
+counter = incrementer()
+
+for i in range(20):
+	print(next(counter))
+
